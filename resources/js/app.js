1ced1549aa393d57da34c9e7a1c84d8447a592cc
phyo min hein <phyominhein321@gmail.com>
2024-02-17 23:51:16 +0630

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+import Example from "./components/Example.vue";
+
+// const app = createApp({
+//     components: {
+//         Example,
+//     }
+// });
+
+const app = createApp({})
+
+app.component('example', Example)
+
+app.mount("#app");
